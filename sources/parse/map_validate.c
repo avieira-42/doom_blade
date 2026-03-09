62199c515f4edec58ebce3c89a87aec57e8732f7
avieira-42 <vieiramendesalex@gmail.com>
2026-03-09 22:26:09 +0000

    is enclosed function done

--- sources/parse/map_validate.c
+++ sources/parse/map_validate.c
@@ -1,32 +1,27 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "cub_utils.h"
-#include "cub_structs.h"
+#include "cub_types.h"
 
-static
-int	stt_cub_flood_fill(t_mat8 map, int32_t x, int32_t y)
+int	cub_is_map_enclosed(t_mat8 map)
 {
+	size_t	x;
+	size_t	y;
 	uint8_t	block;
 
-	if (x >= map.cols || x <= -1 || y >= map.rows || y <= -1)
-		return (-1);
-	block = map.ptr[map.cols * x + map.rows * y];
-	if (block != 0)
-		return (0);
-	map.ptr[map.cols * x + map.rows * y] = '1';
-	if (stt_cub_flood_fill(map, x + 1, y) == -1)
-		return (-1);
-	if (stt_cub_flood_fill(map, x - 1, y) == -1)
-		return (-1);
-	if (stt_cub_flood_fill(map, x, y + 1) == -1)
-		return (-1);
-	if (stt_cub_flood_fill(map, x, y - 1) == -1)
-		return (-1);
+	y = 0;
+	while (y < map.rows)
+	{
+		x = 0;
+		while(x < map.cols)
+		{
+			block = map.ptr[x + map.cols * y];
+			if ((x == 0 || x == map.cols - 1 || y == 0 || y == map.rows - 1)
+					&& block != '1')
+				return (-1);
+			x++;
+		}
+		y++;
+	}
 	return (0);
-}
-
-int	cub_is_map_enclosed(t_mat8 map, t_vec2 player_pos)
-{
-	return (stt_cub_flood_fill(map, player_pos.x.i, player_pos.y.i));
 }
